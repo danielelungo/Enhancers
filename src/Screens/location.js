871ba13fde6f95c485c773e9e9d13ebdae0b07fd
Daniele Lungo <daniele.lungo@epicura.it>
2022-07-21 19:26:05 +0200

    functional location screen

--- src/Screens/location.js
+++ src/Screens/location.js
@@ -1,7 +1,40 @@
+import { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
+import * as Location from "expo-location";
+import HourlyAndDailyWeather from "../Components/hourlyAndDailyWeather";
 
 const LocationScreen = () => {
-  return <Text>POSition</Text>;
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+
+      const location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  return (
+    <View>
+      {errorMsg || location ? (
+        <Text>{errorMsg}</Text>
+      ) : (
+        <Text>"Waiting for permission..."</Text>
+      )}
+      {location && (
+        <HourlyAndDailyWeather
+          lat={location?.coords?.latitude}
+          lon={location?.coords?.longitude}
+        />
+      )}
+    </View>
+  );
 };
 
 export default LocationScreen;
