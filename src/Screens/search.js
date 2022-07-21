b6d9eb0757bf6f7e76ed0b57ecc8b8c5f2d2a872
Daniele Lungo <daniele.lungo@epicura.it>
2022-07-21 19:26:21 +0200

    functional search screen

--- src/Screens/search.js
+++ src/Screens/search.js
@@ -1,7 +1,40 @@
-import { View, Text, Button } from "react-native";
+import { useState } from "react";
+import { View, Text, Button, TextInput } from "react-native";
+import { api } from "../constants/api";
+import HourlyAndDailyWeather from "../Components/hourlyAndDailyWeather";
 
 const SearchScreen = () => {
-  return <Text>SEARCH</Text>;
+  const [text, setText] = useState("");
+  const [weather, setWeather] = useState();
+
+  const search = (evt) => {
+    fetch(`${api.url}weather?q=${text}&units=metric&APPID=${api.key}`)
+      .then((res) => res.json())
+      .then((result) => {
+        setText("");
+
+        setWeather(result);
+      });
+  };
+  console.log("MAREMMA", weather);
+  return (
+    <View>
+      <TextInput
+        placeholder="search a city..."
+        value={text}
+        onChangeText={setText}
+        onSubmitEditing={search}
+      />
+      <Text>{weather?.weather?.[0].main}</Text>
+      {/* <Text>{Math.round(weather?.main?.temp)}°</Text> */}
+      {weather ? (
+        <HourlyAndDailyWeather
+          lat={weather.coord.lat}
+          lon={weather.coord.lon}
+        />
+      ) : null}
+    </View>
+  );
 };
 
 export default SearchScreen;
