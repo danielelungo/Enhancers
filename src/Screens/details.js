bf89a5ee8b41b416021d86b2aa06ffec3b66e4d9
Daniele Lungo <daniele.lungo@epicura.it>
2022-07-21 19:25:46 +0200

    add hourlyanddailyweather to details

--- src/Screens/details.js
+++ src/Screens/details.js
@@ -1,50 +1,20 @@
-import { View, Text, Button } from "react-native";
-import { useState, useEffect } from "react";
-import { api } from "../api/api";
-import DailyCard from "../Components/dailyCard";
+import { View, Text, Button, ScrollView } from "react-native";
+import { completeData } from "../constants/datesAndTimes";
+import HourlyAndDailyWeather from "../Components/hourlyAndDailyWeather";
 
-const DetailsScreen = ({ navigation, route }) => {
-  const [weather, setWeather] = useState([]);
+const DetailsScreen = ({ route }) => {
   const { cityName, lat, lon } = route.params;
 
-  const searchDaily = () => {
-    fetch(
-      `${api.url}onecall?lat=${lat}&lon=${lon}&exclude=minutely,alerts&units=metric&appid=${api.key}`
-    )
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setWeather(data);
-      });
-  };
-
-  useEffect(() => {
-    searchDaily();
-  }, []);
-
-  console.log("=>", weather);
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>{cityName}</Text>
-      <Text>{lat}</Text>
-      <Text>{lon}</Text>
-      {weather.hourly &&
-        weather?.hourly
-          ?.slice(0, 12)
-          .map((hour) => <Text key={hour.dt}>{hour.temp}</Text>)}
-      <Text>PORCO</Text>
-      {weather.daily &&
-        weather?.daily
-          ?.slice(1, 8)
-          .map((dailyData) => (
-            <DailyCard key={dailyData.dt} dailyData={dailyData} />
-          ))}
-      <Button
-        title="Go to Details... again"
-        onPress={() => navigation.navigate("Details")}
-      />
-    </View>
+    <ScrollView>
+      <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+        <Text>{cityName}</Text>
+        <Text>{completeData}</Text>
+        <Text>{lat}</Text>
+        <Text>{lon}</Text>
+        <HourlyAndDailyWeather lat={lat} lon={lon} />
+      </View>
+    </ScrollView>
   );
 };
 
