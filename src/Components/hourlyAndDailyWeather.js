36fe8012c6525ddec25bb8183ad7c775a5a194f7
Daniele Lungo <daniele.lungo@epicura.it>
2022-07-22 00:14:58 +0200

    add styled components in hourlyAndDaily

--- src/Components/hourlyAndDailyWeather.js
+++ src/Components/hourlyAndDailyWeather.js
@@ -1,13 +1,12 @@
-import { View, Text, Button, ScrollView } from "react-native";
+import { ScrollView, Image } from "react-native";
 import { useState, useEffect } from "react";
 import { api } from "../constants/api";
 import DailyCard from "./dailyCard";
 import HourlyCard from "./hourlyCard";
-import { completeData } from "../constants/datesAndTimes";
+import styled from "styled-components";
 
 const HourlyAndDailyWeather = ({ lat, lon }) => {
-  console.log("lon", lat, lon);
-  const [weather, setWeather] = useState([]);
+  const [weather, setWeather] = useState();
 
   const searchDaily = () => {
     fetch(
@@ -23,19 +22,59 @@ const HourlyAndDailyWeather = ({ lat, lon }) => {
 
   useEffect(() => {
     searchDaily();
-  }, [lon, lat]);
+  }, [lat, lon]);
+
   return (
     <ScrollView>
-      <HourlyCard hourlyData={weather?.hourly} />
-      <Text>PORCO</Text>
-      {weather.daily &&
-        weather?.daily
-          ?.slice(1, 8)
-          .map((dailyData) => (
+      <Wrapper>
+        <Weather>{weather?.current?.weather?.[0].main}</Weather>
+        <Flex>
+          <Image
+            source={{
+              uri: `http://openweathermap.org/img/wn/${weather?.current?.weather?.[0]?.icon}@4x.png`,
+            }}
+            style={{ width: 150, height: 150 }}
+          />
+          <Temp>{Math.round(weather?.current?.temp)}°</Temp>
+        </Flex>
+        <HourlyCard hourlyData={weather?.hourly} />
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          style={{ marginTop: 20 }}
+        >
+          {weather?.daily?.slice(1, 8).map((dailyData) => (
             <DailyCard key={dailyData.dt} dailyData={dailyData} />
           ))}
+        </ScrollView>
+      </Wrapper>
     </ScrollView>
   );
 };
+const Wrapper = styled.View`
+  align-items: center;
+  justify-content: center;
+  background-color: #6090ed;
+  height: 100%;
+`;
+const Flex = styled.View`
+  margin: 30px;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Temp = styled.Text`
+  color: white;
+  font-size: 60px;
+  font-weight: bold;
+  margin-left: 20px;
+`;
+
+const Weather = styled.Text`
+  color: white;
+  font-size: 20px;
+`;
 
 export default HourlyAndDailyWeather;
