3fdf6150896e4d60a386e4e977b4b461449dafa1
Daniele Lungo <daniele.lungo@epicura.it>
2022-07-21 20:46:49 +0200

    add styled components

--- src/Components/cityCard.js
+++ src/Components/cityCard.js
@@ -2,6 +2,7 @@ import { View, Text, Button, Image, TouchableOpacity } from "react-native";
 import { useState, useEffect } from "react";
 import { api } from "../constants/api";
 import { completeData } from "../constants/datesAndTimes";
+import styled from "styled-components";
 
 const CityCard = ({ cityName, navigation, timeZone }) => {
   const [weather, setWeather] = useState([]);
@@ -29,20 +30,70 @@ const CityCard = ({ cityName, navigation, timeZone }) => {
         })
       }
     >
-      <Text>{cityName}</Text>
-      <Text>{timeZone}</Text>
-      <Text>{completeData}</Text>
-      {weather?.weather?.[0].icon ? (
-        <Image
-          source={{
-            uri: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`,
-          }}
-          style={{ width: 300, height: 300 }}
-        />
-      ) : null}
-      <Text>{Math.round(weather?.main?.temp)}°</Text>
+      <View
+        style={{
+          backgroundColor: "#6090ED",
+          display: "flex",
+          flexDirection: "row",
+          borderRadius: 15,
+          margin: 20,
+        }}
+      >
+        <CardText>
+          <CityName>{cityName}</CityName>
+          <Date>{completeData}</Date>
+          <Time>{timeZone}</Time>
+        </CardText>
+
+        {weather?.weather?.[0].icon ? (
+          <AlignedView>
+            <Image
+              source={{
+                uri: `http://openweathermap.org/img/wn/${weather.weather[0].icon}@4x.png`,
+              }}
+              style={{ width: 130, height: 130 }}
+            />
+          </AlignedView>
+        ) : null}
+        <AlignedView>
+          <Temp>{Math.round(weather?.main?.temp)}°</Temp>
+        </AlignedView>
+      </View>
     </TouchableOpacity>
   );
 };
-
 export default CityCard;
+
+const CardText = styled.View`
+  margin: 15px 0px 15px 15px;
+`;
+
+const CityName = styled.Text`
+  color: white;
+  font-size: 30px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+
+const Date = styled.Text`
+  color: white;
+  font-size: 15px;
+  font-weight: bold;
+  margin-bottom: 10px;
+`;
+const Time = styled.Text`
+  color: white;
+  margin-bottom: 10px;
+`;
+
+const Temp = styled.Text`
+  color: white;
+  font-size: 40px;
+  font-weight: bold;
+  margin-left: 20px;
+`;
+
+const AlignedView = styled.View`
+  align-items: center;
+  justify-content: center;
+`;
